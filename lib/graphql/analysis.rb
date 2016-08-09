a4697fb0d6de2462c89d891d49a7bd791556074b
Garen Torikian <gjtorikian@gmail.com>
2016-08-09 10:42:28 -0700

    deploy site to gh-pages (automatic)

--- lib/graphql/analysis.rb
+++
@@ -1,5 +0,0 @@
-require "graphql/analysis/max_query_complexity"
-require "graphql/analysis/max_query_depth"
-require "graphql/analysis/query_complexity"
-require "graphql/analysis/query_depth"
-require "graphql/analysis/analyze_query"
